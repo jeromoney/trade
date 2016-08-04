9a6961435b511756722720d1dc83fd3289635ed1
jeromoney <testuser@example.com>
2016-08-04 15:12:46 +0000

    add hash function

--- trade.c
+++ trade.c
@@ -8,6 +8,10 @@
 #include <math.h>
 #include <limits.h>
 #include <assert.h>
+#include <stdint.h>
+
+
+
 
 #include <sys/resource.h>
 #include <sys/time.h>
@@ -15,6 +19,64 @@
 #undef getrusage
 
 
+const int HASH_TABLE_SIZE = 1000;
+
+// http://www.azillionmonkeys.com/qed/hash.html
+#undef get16bits
+#if (defined(__GNUC__) && defined(__i386__)) || defined(__WATCOMC__) \
+  || defined(_MSC_VER) || defined (__BORLANDC__) || defined (__TURBOC__)
+#define get16bits(d) (*((const uint16_t *) (d)))
+#endif
+
+#if !defined (get16bits)
+#define get16bits(d) ((((uint32_t)(((const uint8_t *)(d))[1])) << 8)\
+                       +(uint32_t)(((const uint8_t *)(d))[0]) )
+#endif
+
+uint32_t SuperFastHash (const char * data, int len) {
+uint32_t hash = len, tmp;
+int rem;
+
+    if (len <= 0 || data == NULL) return 0;
+
+    rem = len & 3;
+    len >>= 2;
+
+    /* Main loop */
+    for (;len > 0; len--) {
+        hash  += get16bits (data);
+        tmp    = (get16bits (data+2) << 11) ^ hash;
+        hash   = (hash << 16) ^ tmp;
+        data  += 2*sizeof (uint16_t);
+        hash  += hash >> 11;
+    }
+
+    /* Handle end cases */
+    switch (rem) {
+        case 3: hash += get16bits (data);
+                hash ^= hash << 16;
+                hash ^= ((signed char)data[sizeof (uint16_t)]) << 18;
+                hash += hash >> 11;
+                break;
+        case 2: hash += get16bits (data);
+                hash ^= hash << 11;
+                hash += hash >> 17;
+                break;
+        case 1: hash += (signed char)*data;
+                hash ^= hash << 10;
+                hash += hash >> 1;
+    }
+
+    /* Force "avalanching" of final 127 bits */
+    hash ^= hash << 3;
+    hash += hash >> 5;
+    hash ^= hash << 4;
+    hash += hash >> 17;
+    hash ^= hash << 25;
+    hash += hash >> 6;
+
+    return hash;
+}
 
 // prototype
 double calculate(const struct rusage* b, const struct rusage* a);
@@ -369,7 +431,6 @@ void aStar(int start[] , int goal[] , long long unsigned int minTradeTime , int 
                     long long unsigned int fScore = tentative_gScore + heuristicVal;
 
                     if ((notInOpenSet) &&  (fScore <= maxTradeTime * (N - 1))){
-                    //if ((notInOpenSet) && (fScore <= maxTradeTime * (N - 1))){
                         node * neighborNode = malloc(sizeof(node));
                         neighborNode -> state = neighborArray;
                         neighborNode -> prevpath = current;
